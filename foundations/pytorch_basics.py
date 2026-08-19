921b307fa1cdb93aab60dde188b4643cf38f1a63
Samarjit Debnath <45074297+SamarjitDebnath@users.noreply.github.com>
2026-08-19 23:23:35 +0530

    Update PyTorch basics (PyTorch 1/4)

--- foundations/pytorch_basics.py
+++ foundations/pytorch_basics.py
@@ -2,27 +2,25 @@ import torch
 import torch.nn
 from torchtyping import TensorType
 
-# Helpful functions:
-# https://pytorch.org/docs/stable/generated/torch.reshape.html
-# https://pytorch.org/docs/stable/generated/torch.mean.html
-# https://pytorch.org/docs/stable/generated/torch.cat.html
-# https://pytorch.org/docs/stable/generated/torch.nn.functional.mse_loss.html
-
-# Round your answers to 4 decimal places using torch.round(input_tensor, decimals = 4)
+# Round all answers to 4 decimal places: torch.round(tensor, decimals=4)
 class Solution:
     def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
-        # torch.reshape() will be useful - check out the documentation
+        # Reshape (M, N) tensor to (M*N/2, 2)
+        # Use torch.reshape(tensor, new_shape)
         m, n = to_reshape.shape
-        return torch.round(torch.reshape(to_reshape, shape=[(m*n)//2, 2]), decimals=4)
+        return torch.round(torch.reshape(to_reshape, shape=[(m * n) // 2, 2]), decimals=4)
 
     def average(self, to_avg: TensorType[float]) -> TensorType[float]:
-        # torch.mean() will be useful - check out the documentation
+        # Compute column-wise mean (average across rows)
+        # Use torch.mean(tensor, dim=0)
         return torch.round(torch.mean(to_avg, dim=0), decimals=4)
 
     def concatenate(self, cat_one: TensorType[float], cat_two: TensorType[float]) -> TensorType[float]:
-        # torch.cat() will be useful - check out the documentation
+        # Join two tensors side-by-side along dim=1
+        # Use torch.cat((a, b), dim=1)
         return torch.round(torch.cat((cat_one, cat_two), dim=1), decimals=4)
 
     def get_loss(self, prediction: TensorType[float], target: TensorType[float]) -> TensorType[float]:
-        # torch.nn.functional.mse_loss() will be useful - check out the documentation
-        return torch.round(torch.nn.functional.mse_loss(input=prediction, target=target), decimals=4)
+        # Compute Mean Squared Error between prediction and target
+        # Use torch.nn.functional.mse_loss(prediction, target)
+        return torch.round(torch.nn.functional.mse_loss(prediction, target), decimals=4)
